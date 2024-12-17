98dd496386e2641a217d7e83746d20b9bb097157
Matthew <matthewjwright07@gmail.com>
2024-12-17 17:31:14 -0600

    changed some text

--- componets/AboutAbout.js
+++ componets/AboutAbout.js
@@ -14,7 +14,7 @@ export default function AboutAbout() {
         <h1 className="display-5 fw-bold text-body-emphasis">We Wanted To Make A Differnce</h1>
         <div className="col-lg-6 mx-auto">
           <p className="lead mb-4">
-            Our team simply wanted to make the world a better place, one step at a time. Though this step may be small, we can sleep at night knowing that we have done our best.
+          At Pinzo, we make it easy to discover what's around you—based on your current location. No frills, just the info you need, right when you need it.
           </p>
           <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
           </div>
