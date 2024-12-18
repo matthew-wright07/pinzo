0bca993aabe89944a3547fcc4b4a0ae4df75b0c2
Matthew <matthewjwright07@gmail.com>
2024-12-18 13:26:33 -0600

    fixed google ads

--- app/layout.js
+++ app/layout.js
@@ -24,6 +24,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5455709376701219"
+     crossorigin="anonymous"></script>
+      </head>
       <body>{children}</body>
     </html>
   );
