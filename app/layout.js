240f46f6c7a378aa4488e796ab468e7aeb153e36
Matthew <matthewjwright07@gmail.com>
2024-12-17 21:50:09 -0600

    added favicon

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Export metadata directly in the layout file
+export const metadata = {
+  title: 'The Pinzo | Get Your Current Location',
+  description: 'Click the button to instantly retrieve your current location.',
+  keywords: 'current location, geolocation, find my location, location button',
+  author: 'The Pinzo',
+  openGraph: {
+    title: 'The Pinzo | Get Your Current Location',
+    description: 'Click the button to instantly retrieve your current location.',
+    image: '/favicon.png', // Update this to the correct path
+    url: 'https://www.thepinzo.com',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    image: '/favicon.png', // Update this to the correct path
+  },
+  icons: {
+    icon: '/favicon.png', // Make sure your favicon is placed here
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5455709376701219"
-     crossorigin="anonymous"></script>
-      </head>
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
